8cfa4b74189aeece58a4e1e66641dacc4431e8c6
khaueviana <khaueviana@multconnect.com.br>
2016-07-05 22:25:00 -0300

    first commit

---
+++ KV.DelegateEvent/Figura.cs
@@ -0,0 +1,39 @@
+﻿namespace KV.DelegateEvent
+{
+    public class Figura
+    {
+        public delegate void FiguraHandler(string msg);
+
+        public static event FiguraHandler Invertido;
+
+        public Figura(float a, float b, float c)
+        {
+            m_xPos = a;
+            m_yPos = b;
+            m_zPos = c;
+        }
+
+        public void InverterX()
+        {
+            m_xPos = -m_xPos;
+        }
+
+        public void InverterY()
+        {
+            m_yPos = -m_yPos;
+        }
+
+        public void InverterZ()
+        {
+            m_zPos = -m_zPos;
+
+            Invertido("invertida pelo eixo Z!");
+        }
+
+        private float m_xPos = 0;
+
+        private float m_yPos = 0;
+
+        private float m_zPos = 0;
+    }
+}
---
+++ KV.DelegateEvent/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace KV.DelegateEvent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Figura Minhafigura = new Figura(10, 20, 30);
+
+            Figura.Invertido += OnFiguraInvertida;
+
+            Minhafigura.InverterX();
+
+            Console.WriteLine("\r\n");
+
+            Minhafigura.InverterZ();
+        }
+
+        private static void OnFiguraInvertida(string msg)
+        {
+            Console.WriteLine("A figura foi {0}", msg);
+        }
+    }
+}
